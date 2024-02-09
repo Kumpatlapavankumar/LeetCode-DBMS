c6c88e9a5df11e2214a3441d6e5498c251bcccb5
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-02-09 19:36:14 +0530

    Create 1174. Immediate Food Delivery II.sql

---
+++ 1174. Immediate Food Delivery II.sql
@@ -0,0 +1,7 @@
+select 
+ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) as immediate_percentage
+from delivery 
+where (customer_id, order_date) IN
+(select customer_id, min(order_date) as first_order
+from delivery
+group by customer_id)
