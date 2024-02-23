97480f381f689c72217dfe126a23931a5c89577b
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-02-23 16:56:16 +0530

    Create 1484. Group Sold Products By The Date.sql

---
+++ 1484. Group Sold Products By The Date.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select sell_date,count(distinct(product)) as num_sold,
+group_concat(distinct (product) order by product asc separator ',') as products 
+from activities 
+group by sell_date
+order by sell_date asc;
