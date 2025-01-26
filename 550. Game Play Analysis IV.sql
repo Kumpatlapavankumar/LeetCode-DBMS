ac62007de30d02b7586e2bbd65c2294a370f0d37
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-01-26 21:56:16 +0530

    Create 550. Game Play Analysis IV.sql

---
+++ 550. Game Play Analysis IV.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+  ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM
+  Activity
+WHERE
+  (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+  IN (
+    SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id
+  )
